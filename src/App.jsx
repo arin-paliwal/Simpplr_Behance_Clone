1c7e34e9e3dfaa4f5834d680f60544b4daaf785a
Arin Paliwal <paliwalarin09@gmail.com>
2024-06-18 17:56:42 +0530

    Initialised Project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './styles/App.css'
+
+function App() {
+
+  return (
+    <h1 className="text-red-600">
+      Upgrade to Behance Pro today: Get advanced analytics, a custom portfolio
+      website, and more features to grow your creative career.
+    </h1>
+  );
+}
+
+export default App
