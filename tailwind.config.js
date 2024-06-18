a8e23dc1c997c4018647665d0d230a2deeb038ad
Arin Paliwal <paliwalarin09@gmail.com>
2024-06-18 18:24:48 +0530

    Added CSS Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue1: "#003ecb",
+        white1:"#f9f9f9",
+        black1:"#191919",
+        blue2:"0057ff#",
+        white2:"e8e8e8"
+      },
+    },
   },
   plugins: [],
 }
